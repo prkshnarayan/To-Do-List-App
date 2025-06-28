281b3c58cbae463663dd7ae1ed704b8683378bf3
prkshnarayan <jayaprakashbommisetty2580@gmail.com>
2025-06-28 16:37:15 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# Database Model
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Boolean, default=False)
+
+
+# Home Route
+@app.route("/")
+def home():
+    todo_list = Todo.query.all()
+    return render_template("index.html", todo_list=todo_list)
+
+
+# Add Todo
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    if not title or not title.strip():
+        return redirect(url_for("home"))
+    new_todo = Todo(title=title.strip(), complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+# Update Todo
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.get_or_404(todo_id)
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+# Delete Todo
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.get_or_404(todo_id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+# Run App
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
